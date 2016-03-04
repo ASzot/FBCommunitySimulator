c45430e7f7e1eb0b9bad21730648dd6223377a11
Andrew Szot <me@andrewszot.com>
2016-03-04 21:54:55 +0000

    Added the basic structure for working with the database in the main loop

--- MySQLHelper/DbMgr.py
+++ MySQLHelper/DbMgr.py
@@ -1,10 +1,14 @@
 from FacebookData.ProfileMiner import DbMgr.py
-from MySQLHelper.SQLConnect
+from MySQLHelper.SQLConnect import SQLConnect
 
 class DbMgr: 
 	def __init__(self, sqlConnector):
 		self.connector = sqlConnector
 
 	def saveUserProfile(self, userProfile):
-		#TOOD:
+		#TODO:
 		# Put the code for saving a user's profile here.
+
+	def loadAllUserProfiles(self):
+		#TODO:
+		# Load all of the user profiles and return them in an arrray
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from FacebookMining.FacebookObject import FacebookObject
 from FacebookMining.ProfileMiner import ProfileMiner
 from selenium import webdriver
+from MySQLHelper.SQLConnect import SQLConnect
+from MySQLHelper.DbMgr import DbMgr
 import time
 import pickle
+
+
+def collectUserInfo(dbMgr):
+	outputFile = open("friends_output.txt", "r+")
+	userFriends = pickle.load(outputFile)
+	profileMiner = ProfileMiner(driver)
+
+	for userFriend in userFriends:
+		profileMiner.mineProfile(userFriends[0])
+		dbMgr.saveUserProfile(profileMiner.getUserProfile
+	# To avoid making too many calls to facebook too fast and seeming like a robot. 
+	time.sleep(3)
+
+def run(dbMgr):
+	
+	
+
+
+sqlConnect = SQLConnect() 
+dbMgr = DbMgr(sqlConnect)
 
 driver = webdriver.Firefox()
 
@@ -11,15 +33,12 @@ fbObj = FacebookObject(driver, "USERNAME", "PASSWORD")
 
 fbObj.login()
 
-time.sleep(3)
+# Either mine the data and save it to the database.
+# This is creating the database and setting up the necessary data.
+#collectUserInfo(dbMgr)
 
-outputFile = open("friends_output.txt", "r+")
-userFriends = pickle.load(outputFile)
-
-profileMiner = ProfileMiner(driver)
-profileMiner.mineProfile(userFriends[0])
-
+# Or use the data already present in the database to load and update the users 
+#run(dbMgr)
 
 time.sleep(3)
-
 driver.close()
